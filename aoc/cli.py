983054b367bcd2875d7296620e7507ec5d86ccba
JP <dragonginger10@gmail.com>
2024-03-04 12:32:13 -0600

    formatting

--- aoc/cli.py
+++ aoc/cli.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+from typing import Annotated, Optional
 
 import typer
+from aocd import get_data
 
 from aoc.day_1 import day_1_func, day_1_p2
 from aoc.day_2 import day_2_p1, day_2_p2
@@ -10,15 +12,47 @@ from aoc.utils import file_to_string
 app = typer.Typer()
 
 @app.command(name="one")
-def day_1(input: Path) -> None:
-    data = file_to_string(input)
+def day_1(input: Annotated[
+    Optional[Path], 
+    typer.Argument(
+        help="Optional file for input"
+    )
+] = None) -> None:
+    if input is None:
+        data = get_data(day=1, year=2022)
+    else:
+        data = file_to_string(input)
     answer = day_1_func(data)
     p2_answer = day_1_p2(data)
     render(answer, p2_answer, "one")
 
 @app.command(name="two")
-def day_2(input: Path) -> None:
-    data = file_to_string(input)
+def day_2(input: Annotated[
+        Optional[Path],
+        typer.Argument(
+        help="Optional file for input",
+    )
+    ] = None) -> None:
+    if input:
+        data = file_to_string(input)
+    else:
+        data = get_data(day=2, year=2022)
     answer = day_2_p1(data)
     p2_answer = day_2_p2(data)
     render(answer, p2_answer, "two")
+
+@app.command(name="three")
+def day_3(input: Annotated[
+        Optional[Path],
+        typer.Argument(
+        help="Optional file for input",
+    )
+    ] = None) -> None:
+    if input:
+        data = file_to_string(input)
+    else:
+        data = get_data(day=3, year=2022)
+
+    p1 = None
+    p2 = None
+    render(p1, p2, "Three")
--- aoc/render.py
+++ aoc/render.py
@@ -4,7 +4,7 @@ from rich.panel import Panel
 
 console = Console(color_system="truecolor")
 
-def render(p1: type[str|int], p2: type[str|int], day: int) -> None:
+def render(p1: type[str|int], p2: type[str|int], day: str) -> None:
     string = f"Part one: [green]{p1}[cyan]\nPart two: [green]{p2}"
     panel = Panel.fit(Align(string, "center"),title=f"Solution for Day {day}")
     console.print(panel, style="cyan")
